825ed866d6d398d0eb2860a3666a70b3ad0ceb58
Tyler Peterson <peterson_tyler@apple.com>
2018-01-04 17:21:51 -0500

    Removing package-lock.json to avoid deep dependency vulnerability.

--- test/examples/js/demo.js
+++ test/examples/js/demo.js
@@ -3,18 +3,19 @@
 var vph = require('./../../../');
 
 var rulerInitial = document.getElementById('ruler-initial');
-var rulerLabel = document.getElementById('ruler-label');
-var windowInitial = document.getElementById('win-initial');
+// var rulerLabel = document.getElementById('ruler-label');
+// var windowInitial = document.getElementById('win-initial');
 var windowLabel = document.getElementById('win-label');
 
 var measure = function () {
 	rulerInitial.textContent = vph() + 'px';
-	windowInitial.textContent = window.innerHeight + 'px';
+	// windowLabel.textContent = window.innerHeight + 'px';
+	// windowInitial.textContent = window.innerHeight + 'px';
 	update();
 };
 
 var update = function () {
-	rulerLabel.textContent = vph() + 'px';
+	// rulerLabel.textContent = vph() + 'px';
 	windowLabel.textContent = window.innerHeight + 'px';
 };
 
